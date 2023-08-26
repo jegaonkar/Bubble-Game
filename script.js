46e4aa9bf5170d75600339f97eb2b23b437d8fd1
Akshay jegaonkar <jegaonkar3@gmail.com>
2023-08-26 15:52:42 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,43 @@
+let bubble = '';
+let timer = 6;
+let click;
+let score = 0;
+function scoreAddition(){
+  score += 10;
+  document.querySelector("#scorebox").textContent = score;
+}
+function clickvalue(){
+ click = Math.floor(Math.random()*10);
+ document.querySelector("#clickval").textContent = click;
+}
+function bubbleMaker(){
+  for( let i=0; i<=167; i++){
+    let bubbleNumber = Math.floor(Math.random()*10);
+    bubble += `<div class="bubble">${bubbleNumber}</div>`;
+  }
+  document.querySelector("#box2").innerHTML =  bubble;
+}
+function runtimer(){
+  let timerend = setInterval(() => {
+    if(timer > 0){
+    timer--;
+    document.querySelector('#timer').textContent = timer;
+    }
+    else{
+      clearInterval(timerend);
+      document.querySelector("#box2").innerHTML = " <h1> Game Over </h1>"
+    }
+  }, 1000);
+}
+document.querySelector('#box2').addEventListener("click" ,function(insideInfo){
+ let clickNum = Number(insideInfo.target.textContent);
+ if(clickNum == click){
+   scoreAddition()
+   clickvalue();
+   bubbleMaker();
+}
+})
+
+clickvalue();
+bubbleMaker();
+runtimer();
